e7673faaf8c916a863dca35ec1b0f3db3ea7862d
ramin hamza <obssa77@gmail.com>
2020-12-16 17:15:13 +0300

    commit

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,3 +1,13 @@
 //list of main orders
 //list of sub orders
 //submit button
+import React, { Component } from "react";
+
+class Menu extends Component {
+  state = {};
+  render() {
+    return null;
+  }
+}
+
+export default Menu;
